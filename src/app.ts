61ec1a57a72d5fc9674eb95bfddfe5375341f000
Gabriel <gabrielcemp@gmail.com>
2021-09-13 16:22:10 -0300

    setup deployment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import './database'
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 export { app }
--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ const {
 } = mongoConfig
 
 if (`${process.env.NODE_ENV}` !== 'test') {
-  mongoose.connect(`mongodb+srv://${username}:${password}@node.6dja1.mongodb.net/${database}?retryWrites=true&w=majority`)
+  mongoose.connect(`mongodb+srv://${username}:${password}@node-jwt-auth.6dja1.mongodb.net/${database}?retryWrites=true&w=majority`)
     .then(() => console.log('Database connected'))
     .catch((err) => console.log(`Database error ${err}`))
 }
